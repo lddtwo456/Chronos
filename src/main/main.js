f3ef49fb4b6a9c2aefa99ca9a0e484326022a60c
Luke Davis <lddtwo45@gmail.com>
2026-05-08 09:06:55 -0400

    test window

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,14 @@
-console.log("testing smile");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadFile("./src/renderer/index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
