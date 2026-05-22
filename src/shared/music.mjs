db5452fc1ce5e6bfea646b9c6600fb2c6abe604a
Luke Davis <lddtwo45@gmail.com>
2026-05-22 08:54:11 -0400

    beeeep

--- src/shared/music.mjs
+++ src/shared/music.mjs
@@ -1,14 +1,30 @@
 export { Music };
 
 class Music {
-    constructor(defaultBPM, beatStamps, noteDistances) {
+    constructor(defaultBPM, beatStamps, noteDistances, notation) {
         this.defaultBPM = defaultBPM;
         this.beatStamps = beatStamps;
         this.beatDistances = noteDistances;
+        this.notation = notation;
     }
 
     static async fromMidiFile(fileName) {
         const data = await window.api.parseFile(fileName);
-        return new Music(60000000/data.microsecondsPerBeat, data.beatStamps, data.noteDistances);
+        return new Music(60000000/data.microsecondsPerBeat, data.beatStamps, data.noteDistances, null);
+    }
+
+    static generate(args) {
+        // generate the stuff with ur stuff
+        return new Music(120, null, null, null);
+    }
+
+    getTimestamps(bpm) {
+        const beatDuration = 1/(bpm*60);
+        return this.beatStamps.map(beat => beat*beatDuration);
+    }
+
+    getDurations(bpm) {
+        const beatDuration = 1/(bpm*60);
+        return this.beatDistances.map(distance => distance*beatDuration);
     }
 }
