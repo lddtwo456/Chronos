4c4b5577b068423a47356369664d3f40b5d9fcb5
lddtwo456 <lddtwo45@gmail.com>
2026-06-03 01:01:29 -0400

    get rid of some logging

--- src/main/services/inputsHandler.js
+++ src/main/services/inputsHandler.js
@@ -5,12 +5,10 @@ let previousKeys = {};
 
 uIOhook.on('keydown', (e) => {
     keys[e.keycode] = true;
-    console.log('down', e.keycode);
 });
 
 uIOhook.on('keyup', (e) => {
     keys[e.keycode] = false;
-    console.log('up', e.keycode);
 });
 
 uIOhook.start();
@@ -18,7 +16,6 @@ uIOhook.start();
 function getPressed() {
     let pressed = false;
     for (const key of Object.keys(keys)) {
-        console.log(key, keys[key], previousKeys[key]);
         if (keys[key] && !previousKeys[key]) {
             pressed = true;
         }
